7592a275104bcf520b8038c9dae541f448d99a5e
Aditya Chaturvedi <aditya@Adityas-MacBook-Pro.local>
2026-07-29 04:41:49 +0530

    Add Secret VIP Executive Suite and 1-Tap WhatsApp Dues Broadcaster unlocked by long-pressing top header logo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'anshu-coaching-v32';
+const CACHE_NAME = 'anshu-coaching-v33';
 
 // Force Service Worker to activate immediately on updates
 self.addEventListener('install', (event) => {
